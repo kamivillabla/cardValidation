440f02072affdee5bdc267108ee4a768183698da
kamila villablanca <kvillablancabaez@gmail.com>
2021-12-16 13:01:39 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,42 @@
+# VerifyCard 
+
+## 1. RESUMEN DEL PROYECTO
+
+Verify Card, es una página web donde puedes ingresar el número de tu tarjeta y verificar que sea un número valido.
+
+<img src = "src/img/readme/paginaWebFinal.png">
+
+# 2. Investigación UX:
+## 2.1 ¿Quiénes son los principales usuarios de producto?
+
+Personas mayores de 18 años que tengan acceso a tarjetas de crédito físicas y/o virtuales.
+
+
+## 2.2 ¿Cuáles son los objetivos de estos usuarios en relación con tu producto? 
+
+Buscan una solución para saber si el número de su tarjeta de crédito virtual o física se puede verificar.  
+
+## 2.3 ¿Cómo crees que el producto que estás creando está resolviendo sus problemas? 
+
+Dando la la posibilidad de poder ingresar el número de la tarjeta y por medio del algoritmo de luhn saber si es valida.
+
+## 2.4 Prototipo en papel.
+
+<img src = "src/img/readme/prototipoPapel.jpg">
+
+
+## 2.5 Feedback recibido.
+
+* datos a poner
+* datos a poner
+
+## 2.6  Prototipo final.
+
+<img src = "src/img/readme/prototipoFinal.png">
+
+# 3. Herramientas de elaboración del proyecto
+
+HTML, CSS, javaScript.
+
+
+
---
+++ src/validator.js
@@ -0,0 +1,59 @@
+
+const validator = {
+    isValid,
+    maskify
+};
+
+// Enmascarar número de tarjeta.
+function maskify(creditCardNumber) {
+    //Por medio de la expresion regular despues del cuarto dígito comienzo a enmascarar con # (Dejando los ultimos cuatro descubiertos)
+    let maskifyString = creditCardNumber.replace(/.(?=.{4})/g, "#");
+    return maskifyString //retorna el reemplazo de los digitos por #
+}
+
+// Validar tarjeta.
+function isValid(creditCardNumber) {
+    // Convierto el string a array, luego lo doy vuelta y mapeo convirtiendo en número el string.
+    let arrayValidation = creditCardNumber.split('').reverse().map(Number);
+    //console.log(arrayValidation)
+
+    const arrayNotmultiplied = arrayValidation.filter(function (_, index) {
+        return index % 2 == 0;
+    });
+    //console.log(`Retorna los números que no se multiplican${arrayNotmultiplied}`)
+
+    const arrayIfMultiplied = arrayValidation.filter(function (_, index) {
+        return index % 2 !== 0;
+    });
+    //console.log(`Retorna los números que se multiplican ${arrayIfMultiplied}`)
+
+    // Multiplica los números x 2 y el resultado si resulta en dos digitos se suman entre si.
+    const arrayMultiplicate = arrayIfMultiplied.map(function (element) {
+        let num = element;
+        num = num * 2;
+        if (num >= 9) {
+            num = num - 9;
+        }
+        return num;
+    });
+    //console.log(`Números multiplicados ${arrayMultiplicate}`)
+
+    const arrayCombine = arrayNotmultiplied.concat(arrayMultiplicate);
+
+    //console.log(`Muestra los dos array juntos ${arrayCombine}`);
+
+    // Sumar todos los números del array
+    let sum = 0;
+    for (let i = 0; i < arrayCombine.length; i++) {
+        sum = sum + arrayCombine[i];
+    }
+    // //console.log(sum)
+
+    if (sum % 10 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export default validator;
